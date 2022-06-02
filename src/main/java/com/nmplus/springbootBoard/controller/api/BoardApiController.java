114f78894a5287f7d4475fb21f50e3e5d55d1363
Oh-Jeonghwan <vmflwmf@gmail.com>
2022-06-02 17:32:36 +0900

    싱크맞추기

--- src/main/java/com/nmplus/springbootBoard/controller/api/BoardApiController.java
+++ src/main/java/com/nmplus/springbootBoard/controller/api/BoardApiController.java
@@ -3,9 +3,6 @@ package com.nmplus.springbootBoard.controller.api;
 import java.security.Principal;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -21,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
